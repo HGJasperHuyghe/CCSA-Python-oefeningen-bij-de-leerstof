a6f2eaf13e8c5343208967c6d8d6e45113cc8ef3
Jasper Huyghe <jasper.huyghe@student.hogent.be>
2025-09-30 12:22:35 +0200

    Adds iterative binary search
    Implements an iterative binary search algorithm.
    
    This provides a basic, functional iterative search.
    Further enhancements may be required for optimization
    or specific use cases.

--- h1_zoeken_en_sorteren/iteratieve_versie_binair_zoeken.py
+++ h1_zoeken_en_sorteren/iteratieve_versie_binair_zoeken.py
@@ -1,3 +1,17 @@
 
-def zoekBinair():
-  pass
+
+def zoek_binair(rij, getal):
+  
+  links = 0
+  rechts = len(rij)-1
+  while links != rechts:
+    midden = (links+rechts)//2 # // gehele deling -> dus afronden naar beneden
+    if rij[midden] < getal: 
+      links = midden +1 # zoeken in rechterhelft
+    else:
+     rechts = midden # zoeken in linkerhelft ( incl midden)
+  #na while: links=rechts: 1 element over
+  if rij[links] == getal:
+    return links
+  else:
+    return -1
