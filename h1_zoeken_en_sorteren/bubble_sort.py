eb9da9501417a8aee19bc75f0ed0c0904dfd076a
Jasper Huyghe <jasper.huyghe@student.hogent.be>
2025-10-07 12:20:42 +0200

    Adds test line
    Adds a test line to verify the correct functionality of the script.

--- h1_zoeken_en_sorteren/bubble_sort.py
+++ h1_zoeken_en_sorteren/bubble_sort.py
@@ -1,14 +1,15 @@
 def bubble_sort(a):
     n = len(a)
-    # buitenste lus: elke iteratie zorgt ervoor dat het grootste element 'naar boven komt'
+
     for i in range(n - 1):
-        # binnenste lus: vergelijkt telkens aangrenzende elementen
+
         for j in range(n - 1, i, -1):
             if a[j - 1] > a[j]:
-                # wissel de twee elementen van plaats
+
                 a[j - 1], a[j] = a[j], a[j - 1]
 
 
 a = [int(x) for x in input().split()]
 bubble_sort(a)
+print('programma werkt') # testline
 print(a)
